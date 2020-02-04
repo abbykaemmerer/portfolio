31152b24480433a6d3e6049fbd966486dc16e687
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2020-02-03 21:10:49 -0600

    Add content

--- index.js
+++ index.js
@@ -17,3 +17,11 @@
 const scroll = new SmoothScroll('.navbar a[href*="#"]', {
     speed: 800
 });
+
+$(function scrollNav(){
+    // eslint-disable-next-line func-names
+    $(document).scroll(function (){
+        var $nav = $("#mainNavbar");
+        $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+    });
+});
