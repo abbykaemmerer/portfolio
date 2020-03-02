4bf7ce182d32bd8a45b84dc43fe4e345eb73977b
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2020-03-01 20:50:39 -0600

    Clean up css

--- components/pages/About.js
+++ components/pages/About.js
@@ -23,5 +23,13 @@ export default (state) => `
     <br>
 </div>
 
+<footer>
+&copy; 2019 Abby Kaemmerer
+<a href="https://github.com/abbykaemmerer" target="_blank">
+<span class="fab fa-github"></span"></a>
+<a href="https://www.linkedin.com/in/abby-kaemmerer-80331766/" target="_blank">
+<span class="fab fa-linkedin"></span></a>
+</footer>
+
 
 `;
