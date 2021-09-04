0ca6b2c4fd2f5107cce2dad0a5d63a7d89cf2fa6
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2021-09-04 10:51:11 -0500

    Update about

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default () => `
 <footer>
-&copy; 2019 Abby Kaemmerer
+&copy; 2021 Abby Kaemmerer
 <a href="https://github.com/abbykaemmerer" target="_blank">
 <span class="fab fa-github"></span"></a>
 <a href="https://www.linkedin.com/in/abby-kaemmerer-80331766/" target="_blank">
--- components/pages/About.js
+++ components/pages/About.js
@@ -3,10 +3,16 @@ export default (state) => `
 <div class="borderz">
     <h1 class="title">${state.title}</h1>
     <br><br>
-    <p>If you'd like to learn a bit about me, continue reading. If not, here is a picture of my <a href='https://res.cloudinary.com/abbykaemmerer/image/upload/v1562688662/Tooth_wyb1cq.jpg'> cat </a>.
-    </p>
     <p class="about">
-        I currently work as a developer in an Agile environment for a IT consulting firm where we work mostly with AngularJS, Java, SQL, JavaScript, HTML, CSS, BitBucket, Jenkins, ALM, and Swagger. After graduating from the University of Missouri, I decided to continue my education by enrolling in a Front-End Development bootcamp. We learned JavaScript, Node.js, HTML, CSS, version control, GitHub, Netlify, and Trello. From my education and professional experience, I have learned how to optimize my time while working with many different teams and people. My experiences have taught me to be persistent, curious and efficient while also maintaining my creativity and passion for web development.
+        Currently, I work as a developer for a IT consulting firm based in St. Louis. My team is contracted to MasterCard where we work mostly with AngularJS, Java, SQL, JavaScript, HTML, CSS, BitBucket, Jenkins, ALM, and Swagger.
+        <br><br>
+        After graduating from the University of Missouri with a degree in Fisheries and Wildlife, I worked as a research technician for a few years before deciding to further my education by enrolling in a Front-End Development bootcamp.
+        <br><br>
+        The SavvyCoders bootcamp was a swift and intense dive into the coding world. We learned JavaScript, Node.js, HTML, CSS, version control, GitHub, Netlify, Trello and ultimately how to put together and code our own projects.
+        <br><br>
+        From my education and professional experiences, I have learned how to optimize my time while working with many different teams and people. My experiences have taught me to be persistent, curious and efficient while also maintaining my creativity and passion for web development.
+    </p>
+    <p>Hopefully you enjoyed reading about my learning endeavors but if you didn't here's a consolation <a href='https://res.cloudinary.com/abbykaemmerer/image/upload/v1562688662/Tooth_wyb1cq.jpg'> cat </a> picture.
     </p>
     <br><br>
     <div>
